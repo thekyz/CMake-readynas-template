ea74b8e7faeebe083a671933eff90216a8b18fc3
Alex Garcia <thekyz@gmail.com>
2012-01-24 00:20:39 +0100

    Added install target Added curl example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <stdio.h>
-
-int main()
+#include <curl/curl.h>
+ 
+int main(void)
 {
-	printf("Test !\n");
-
-	return 0;
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://myepisodes.com/login.php?u=myshows.php");
+    res = curl_easy_perform(curl);
+ 
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  return 0;
 }
