05d6f91cdff06ce889da66f517ebe77ff17fb4e2
Alex Garcia <thekyz@gmail.com>
2012-01-23 23:02:03 +0100

    Initial commit

---
+++ conf/toolchain-sparc-linux.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+# Compilers.
+set(CMAKE_C_COMPILER sparc-linux-gcc)
+set(CMAKE_CXX_COMPILER sparc-linux-g++)
+
+# Target environment.
+set(CMAKE_FIND_ROOT_PATH /usr/sparc-linux)
